bbc742234e8e5c990ff1eb3611a3b95fc19d9a7f
Athanasios Markou <thanasismarko@gmail.com>
2018-03-22 16:08:12 +0000

    feat: Add makeGrid() to create table

--- designs.js
+++ designs.js
@@ -1,13 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Select Size picker
+    const sizePicker = document.querySelector('#sizePicker');
+    // Select Design canvas table
+    const canvas = document.querySelector('#pixelCanvas');
     // Select color input
-    // Select size input
 
     // When size is submitted by the user, call makeGrid()
+    sizePicker.addEventListener('submit', function(e) {
+        makeGrid(e);
+    });
 
-    function makeGrid() {
-
-    // Your code goes here!
-
+    function makeGrid(evt) {
+        // Prevent submit default action.
+        evt.preventDefault();
+        // Select size input
+        const height = sizePicker.querySelector('#inputHeight').value;
+        const width = sizePicker.querySelector('#inputWeight').value;
+        // For loop to make the grid table
+        for (let row = 0; row < height; row++) {
+            canvas.appendChild(document.createElement('tr'));
+            for (let col = 0; col < width; col++) {
+                canvas.lastChild.appendChild(document.createElement('td'));
+            }
+        }
     }
 
 });
